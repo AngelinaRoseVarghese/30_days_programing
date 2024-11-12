2c77c42d07d2292bc6dba7342554d5c071f9ca4d
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2024-11-12 22:41:09 +0530

    Update angelina_day1.py

--- day1/angelina_day1.py
+++ day1/angelina_day1.py
@@ -1,1 +1,5 @@
+Date: 12/11/202
+Author: Angeina Rose Varghese
+Description: The print() function prints the specified message to the screen or other standard output devices.
+The message can be a string or any other object; the object will be converted into a string before being displayed.
 print("I Love Python")
