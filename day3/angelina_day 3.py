ad4321d06e3cf27de41a8bd86445ebde640ea970
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2024-11-13 22:29:44 +0530

    Add files via upload

---
+++ day3/angelina_day 3.py
@@ -0,0 +1,9 @@
+'''
+Date: 13/11/2024
+Author: Angelina Rose Varghese
+Description: To Display the multiplication table for a user-input number.
+'''
+num=int(input("Enter the number: "))
+for i in range(1,11):
+    multiplication=num*i
+    print(multiplication)
