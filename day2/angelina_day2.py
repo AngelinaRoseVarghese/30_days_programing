dc769235c4a18a475503385ad5b9b5c5150a3044
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2024-11-13 23:15:57 +0530

    Update angelina_day2.py

--- day2/angelina_day2.py
+++ day2/angelina_day2.py
@@ -1,3 +1,8 @@
+'''
+Date: 13/11/2024
+Author: Angelina Rose Varghese
+Description: To verify whether the password given by user is strong or not
+'''
 password=str(input("Enter password: "))
 length=len(password)
 if length>7:
