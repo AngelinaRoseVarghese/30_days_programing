37f350f874bc7d102b13ec6c1f3c00827019c571
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2024-11-13 23:14:12 +0530

    Add files via upload

---
+++ day4/angelina_day4.py
@@ -0,0 +1,9 @@
+'''
+Date: 13/11/2024
+Author: Angelina Rose Varghese
+Description:A program that uses a while loop to count down from a number entered by the user to zero.
+'''
+num=int(input("Enter a number"))
+while num>0:
+    num=num-1
+    print(num ,end=" ")
