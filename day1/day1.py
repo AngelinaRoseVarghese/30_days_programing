e802590d91c8cb036912e3433de62ad45916e689
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-11-17 15:52:51 +0530

    Add files via upload

---
+++ day1/day1.py
@@ -0,0 +1,11 @@
+'''
+Author: Jacob Thomas
+Date:17/11/24
+Description:The print() function prints the specified message to the screen or other standard output devices.
+The message can be a string or any other object; the object will be converted into a string before being displayed.
+'''
+
+
+
+
+print("I Love python")
