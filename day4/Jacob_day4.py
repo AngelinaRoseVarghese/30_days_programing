8b5ea2b5225bd281c15d80cd74dfb58ba2f28c2b
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-11-17 17:24:27 +0530

    Add files via upload

---
+++ day4/Jacob_day4.py
@@ -0,0 +1,9 @@
+'''
+Author: Jacob Thomas
+Date:17/11/2024
+Desciption: Write a program that uses a while loop to count down from a number entered by the user to zero.
+'''
+number = int(input("Enter a number"))
+while number>0:
+    number=number-1
+    print(number)
