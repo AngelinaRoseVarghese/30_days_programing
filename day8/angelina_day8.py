b8f385f76664f35cc84c5e2d7e5e3fe826544b86
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2024-11-19 22:34:16 +0530

    Add files via upload

---
+++ day8/angelina_day8.py
@@ -0,0 +1,12 @@
+'''
+Date:14/11/2024
+Author: Angelina Rose Varghese
+Description: A program use to check leap year using if-else to check if a given year is a leap year.
+'''
+year=int(input("Enter the year: "))
+if year%2==0:
+    print(year,"is a leap year")
+else:
+    print(year, "is not a leap year")
+
+
